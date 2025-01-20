b8cdfed400214277e357f79b12195484dfac2ad5
Abhishek Singh <56843616+abhishek100699@users.noreply.github.com>
2025-01-20 17:50:07 +0000

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12233
+let accountEmail = "abhisek@gmail.com "
+var accountPassword = "12345"
+accountCity = "Allahabad"
+
+
+// accountId = 2 // not allowed
+accountEmail = "hc@gmail.com"
+
+accountPassword = 12
+accountCity = "Bangalore"
+
+// prefer not to use var , because of block and function scope
+
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
+
+
+
